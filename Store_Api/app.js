2a3a97fd59e0d0ee216ecc8c9991c761d0776982
Pujan-Pandey <your-email@example.com>
2025-12-11 15:42:56 +0545

    Post Method Updated

--- Store_Api/app.js
+++ Store_Api/app.js
@@ -7,24 +7,45 @@ let readJsonFile = fs.readFileSync('../JSON/product.json', 'utf-8'); // returns 
 let products = JSON.parse(readJsonFile);
 // console.log(products)
 
+
+// get method
 app.get('/api/products', (req, res) => {
     res.json(products);
 });
+
+
+// post method 
 app.use(express.json());
 app.post('/api/products', (req, res) => {
-    let newProduct = req.body;
+    let newId = products.products[products.products.length - 1].id + 1;
+    console.log("newID: ", newId);
+
+
+    let newProduct = Object.assign({ id: newId }, req.body);
     products.products.push(newProduct);
     // console.log(newProduct);
-    fs.writeFileSync('../JSON/product.json', JSON.stringify(products, null, 2), 'utf-8');
-    res.status(201).json({
-        status: "success",
-        products: products
+    fs.writeFile('../JSON/product.json', JSON.stringify(products, null, 2), (err) => {
+        if (err) {
+            return res.status(400).json({
+                status: "failed",
+                message: "An Error Occured!, No product to show.."
+            })
+        }
+        if (!newProduct.name) {
+            return res.status(400).json({
+                status: "failed",
+                message: "An Error Occured!, No product to show.."
+            })
+        }
+        res.status(201).json({
+            status: "success",
+            products: products
+        });
     });
 });
 
 
-
-
+// delete method
 
 
 
